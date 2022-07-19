a25a33158e48da3e070e76ad91e0fc65cfd98c44
MedBen <medbenomar@seznam.czroot>
2022-07-19 21:57:34 +0200

    Ee

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$message="<center> <h1>HEllo World !!!</h1> </center>";
+$message="<center> <h1>Hello World !!!</h1> </center>";
 $message2="<center> <h1>First Azur WebApp</h1> </center>";
 
 echo $message;
